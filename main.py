0fb067b33345d0e3030552c5d1e5d061f0bb024d
robertay <94378311+robyuk@users.noreply.github.com>
2022-06-22 10:51:26 +0000

    changed to rename all files in a dir

--- main.py
+++ main.py
@@ -1,28 +1,11 @@
 from pathlib import Path
 
-# Standard method (without pathlib)
-p1='files/abc.txt'
-with open(p1,'r') as file:
-  print(p1,file.read(),'\n')
+root_dir=Path('files')
+file_paths=root_dir.iterdir()
+#print(list(file_paths))
 
-# With Pathlib:
-p2=Path('files/ghi.txt')
-print(type(p1))
-print(p2.name)
-print(p2.stem)
-print(p2.suffix)
-
-
-if p2.exists():
-  with open(p2,'r') as file:
-    print(file.read(),'\n')
-else:
-  with open(p2,'w') as file:
-    file.write('Content 3')
-
-p3=Path('files')
-print(p3.iterdir())
-for item in p3.iterdir():
-  print(item, type(item))
-
-print(list(p3.iterdir()))
+for path in file_paths:
+  new_filename="new-" + path.stem + path.suffix
+  new_filepath=path.with_name(new_filename) # uses existing path dir
+  print(new_filepath)
+  path.rename(new_filepath)
